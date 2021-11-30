66fb954404f1063da9ae06e6e96f311c00f77d72
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2021-11-30 15:39:13 +0100

    gr

--- app.js
+++ app.js
@@ -165,10 +165,10 @@ $('a').each(function () {
 /* HOME SHUFFLE.JS */
 
 /*function loadallshuffle() { */
-
+/*
 var Shuffle = window.Shuffle;
 var element = document.querySelector('.flex-row');
-//var sizer = element.querySelector('.x-sizer');
+var sizer = element.querySelector('.x-sizer');
 
 var shuffleInstance = new Shuffle(element, {
   itemSelector: '.shuffle-box',
