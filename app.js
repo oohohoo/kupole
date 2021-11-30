ebb9c71f24b0de5c3d9f50c33f48755752ed29a9
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2021-11-30 15:31:06 +0100

    hr

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function closeMenu() {
 100 vh FIX - MOBILE MENU
 ================================================================================
 */
-
+/*
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
