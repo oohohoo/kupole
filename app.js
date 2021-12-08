1c7bec82dde0738282004c277d504fcbde4252e5
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2021-12-08 15:24:13 +0100

    gr

--- app.js
+++ app.js
@@ -329,22 +329,7 @@ Webflow.push(function () {
     var $body = $(document.body);
     var scrollPosition = 0;
 
-    $('[scroll="disable"]').on('click', function () {
-        var oldWidth = $body.innerWidth();
-        scrollPosition = window.pageYOffset;
-        $body.css('overflow', 'hidden');
-        $body.css('position', 'fixed');
-        $body.css('top', `-${scrollPosition}px`);
-        $body.width(oldWidth);
-    });
-    $('[scroll="enable"]').on('click', function () {
-        if ($body.css('overflow') != 'hidden') { scrollPosition = window.pageYOffset; }
-        $body.css('overflow', '');
-        $body.css('position', '');
-        $body.css('top', '');
-        $body.width('');
-        $(window).scrollTop(scrollPosition);
-    });
+
     $('[scroll="both"]').on('click', function () {
         if ($body.css('overflow') !== 'hidden') {
             var oldWidth = $body.innerWidth();
