3531fb94887dc2c74927e4503ce160029d3274f0
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-12-22 18:41:25 +0100

    gr

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 /*$(document).ready(function(){
+*/
+
+/*
+================================================================================
+FOUC CODE
+================================================================================
+*/
+  // Helper function
+let domReady = (cb) => {
+  document.readyState === 'interactive' || document.readyState === 'complete'
+    ? cb()
+    : document.addEventListener('DOMContentLoaded', cb);
+};
+
+domReady(() => {
+  // Display body when DOM is loaded
+  document.body.style.visibility = 'visible';
+});
 
 /*
 ================================================================================
